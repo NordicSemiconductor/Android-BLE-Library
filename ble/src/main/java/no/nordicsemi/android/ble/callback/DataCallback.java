aab4e81d811876c08ea91ff72f420cfac20d1d4a
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-04-13 12:24:54 +0200

    Comment updated

--- ble/src/main/java/no/nordicsemi/android/ble/callback/DataCallback.java
+++ ble/src/main/java/no/nordicsemi/android/ble/callback/DataCallback.java
@@ -6,7 +6,7 @@ public interface DataCallback {
 
 	/**
 	 * Callback received each time the value was read or has changed using notifications or indications.
-	 * @param data the full messaged. If the {@link ValueMerger} was used, this contains the merged result.
+	 * @param data the data received. If the {@link ValueMerger} was used, this contains the merged result.
 	 */
 	void onValueChanged(final @NonNull Data data);
 }
