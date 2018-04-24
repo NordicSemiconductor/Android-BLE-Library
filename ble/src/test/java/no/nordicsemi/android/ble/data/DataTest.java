c8b717dfd585f5451cddb6882ef4ae85f4cf0210
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-04-24 08:29:27 +0200

    More Data tests

--- ble/src/test/java/no/nordicsemi/android/ble/data/DataTest.java
+++ ble/src/test/java/no/nordicsemi/android/ble/data/DataTest.java
@@ -4,7 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+@SuppressWarnings("ConstantConditions")
 public class DataTest {
+
+	@Test
+	public void setValue_SFLOAT_hex() {
+		final Data data = new Data(new byte[2]);
+		data.setValue(10.1f, Data.FORMAT_SFLOAT, 0);
+		assertArrayEquals(new byte[] { 0x65, (byte) 0xF0}, data.getValue());
+	}
 
 	@Test
 	public void setValue_SFLOAT_basic() {
@@ -79,11 +87,17 @@ public class DataTest {
 	}
 
 	@Test
+	public void setValue_FLOAT_hex() {
+		final Data data = new Data(new byte[4]);
+		data.setValue(12345.67f, Data.FORMAT_FLOAT, 0);
+		assertArrayEquals(new byte[] {(byte) 0x87, (byte) 0xD6, (byte) 0x12, (byte) 0xFE}, data.getValue());
+	}
+
+	@Test
 	public void setValue_FLOAT_basic() {
 		final Data data = new Data(new byte[4]);
 		data.setValue(1.0f, Data.FORMAT_FLOAT, 0);
-		final float value = data.getFloatValue(Data.FORMAT_FLOAT, 0);
-		assertEquals(1.0f, value, 0.00);
+		assertArrayEquals(new byte[] { 1, 0, 0, 0}, data.getValue());
 	}
 
 	@Test
@@ -116,5 +130,117 @@ public class DataTest {
 		data.setValue(Float.NaN, Data.FORMAT_FLOAT, 0);
 		final float value = data.getFloatValue(Data.FORMAT_FLOAT, 0);
 		assertEquals(Float.NaN, value, 0.00);
+	}
+
+	@Test
+	public void setValue_UINT8() {
+		final Data data = new Data(new byte[1]);
+		data.setValue(200, Data.FORMAT_UINT8, 0);
+		assertArrayEquals(new byte[] {(byte) 0xC8} , data.getValue());
+	}
+
+	@Test
+	public void getValue_UINT8() {
+		final Data data = new Data(new byte[] {(byte) 0xC8 });
+		final int value = data.getIntValue(Data.FORMAT_UINT8, 0);
+		assertEquals(200, value);
+	}
+
+	@Test
+	public void setValue_SINT8() {
+		final Data data = new Data(new byte[1]);
+		data.setValue(-120, Data.FORMAT_SINT8, 0);
+		assertArrayEquals(new byte[] {(byte) 0x88} , data.getValue());
+	}
+
+	@Test
+	public void getValue_SINT8() {
+		final Data data = new Data(new byte[] { (byte) 0x88 });
+		final int value = data.getIntValue(Data.FORMAT_SINT8, 0);
+		assertEquals(-120, value);
+	}
+
+	@Test
+	public void setValue_UINT16() {
+		final Data data = new Data(new byte[2]);
+		data.setValue(26576, Data.FORMAT_UINT16, 0);
+		assertArrayEquals(new byte[] {(byte) 0xD0, 0x67} , data.getValue());
+	}
+
+	@Test
+	public void getValue_UINT16() {
+		final Data data = new Data(new byte[] {(byte) 0xD0, 0x67 });
+		final int value = data.getIntValue(Data.FORMAT_UINT16, 0);
+		assertEquals(26576, value);
+	}
+
+	@Test
+	public void setValue_SINT16() {
+		final Data data = new Data(new byte[2]);
+		data.setValue(-6192, Data.FORMAT_SINT16, 0);
+		assertArrayEquals(new byte[] {(byte) 0xD0, (byte) 0xE7} , data.getValue());
+	}
+
+	@Test
+	public void getValue_SINT16() {
+		final Data data = new Data(new byte[] { (byte) 0xD0, (byte) 0xE7 });
+		final int value = data.getIntValue(Data.FORMAT_SINT16, 0);
+		assertEquals(-6192, value);
+	}
+
+	@Test
+	public void setValue_UINT24() {
+		final Data data = new Data(new byte[3]);
+		data.setValue(0x010203, Data.FORMAT_UINT24, 0);
+		assertArrayEquals(new byte[] { 0x03, 0x02, 0x01} , data.getValue());
+	}
+
+	@Test
+	public void getValue_UINT24() {
+		final Data data = new Data(new byte[] { 0x03, 0x02, 0x01});
+		final int value = data.getIntValue(Data.FORMAT_UINT24, 0);
+		assertEquals(0x010203, value);
+	}
+
+	@Test
+	public void setValue_SINT24() {
+		final Data data = new Data(new byte[3]);
+		data.setValue(0xfefdfd, Data.FORMAT_UINT24, 0);
+		assertArrayEquals(new byte[] {(byte) 0xFD, (byte) 0xFD, (byte) 0xFE} , data.getValue());
+	}
+
+	@Test
+	public void getValue_SINT24() {
+		final Data data = new Data(new byte[] { (byte) 0xFD, (byte) 0xFD, (byte) 0xFE});
+		final int value = data.getIntValue(Data.FORMAT_UINT24, 0);
+		assertEquals(0xfefdfd, value);
+	}
+
+	@Test
+	public void setValue_UINT32() {
+		final Data data = new Data(new byte[4]);
+		data.setValue(0x01020304, Data.FORMAT_UINT32, 0);
+		assertArrayEquals(new byte[] { 0x04, 0x03, 0x02, 0x01} , data.getValue());
+	}
+
+	@Test
+	public void getValue_UINT32() {
+		final Data data = new Data(new byte[] { 0x04, 0x03, 0x02, 0x01});
+		final int value = data.getIntValue(Data.FORMAT_UINT32, 0);
+		assertEquals(0x01020304, value);
+	}
+
+	@Test
+	public void setValue_SINT32() {
+		final Data data = new Data(new byte[4]);
+		data.setValue(0xfefdfd00, Data.FORMAT_UINT32, 0);
+		assertArrayEquals(new byte[] {(byte) 0x00, (byte) 0xFD, (byte) 0xFD, (byte) 0xFE} , data.getValue());
+	}
+
+	@Test
+	public void getValue_SINT32() {
+		final Data data = new Data(new byte[] { (byte) 0x00, (byte) 0xFD, (byte) 0xFD, (byte) 0xFE});
+		final int value = data.getIntValue(Data.FORMAT_UINT32, 0);
+		assertEquals(0xfefdfd00, value);
 	}
 }
