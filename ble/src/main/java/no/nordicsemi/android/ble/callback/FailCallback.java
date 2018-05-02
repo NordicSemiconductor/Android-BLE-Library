3b594a56af6cf451f0208eddc570eee79125a4d1
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-05-02 15:08:32 +0200

    Comments and formatting

--- ble/src/main/java/no/nordicsemi/android/ble/callback/FailCallback.java
+++ ble/src/main/java/no/nordicsemi/android/ble/callback/FailCallback.java
@@ -4,12 +4,15 @@ import android.bluetooth.BluetoothDevice;
 
 public interface FailCallback {
 	int REASON_REQUEST_FAILED = -1;
+	int REASON_NULL_ATTRIBUTE = -2;
 
 	/**
-	 * A callback invoked when the request has failed with status other than {@link android.bluetooth.BluetoothGatt#GATT_SUCCESS}.
+	 * A callback invoked when the request has failed with status other than
+	 * {@link android.bluetooth.BluetoothGatt#GATT_SUCCESS}.
 	 *
 	 * @param device target device.
-	 * @param status error status code, one of BluetoothGatt#GATT_* constants or {@link #REASON_REQUEST_FAILED}.
+	 * @param status error status code, one of BluetoothGatt#GATT_* constants or
+	 *               {@link #REASON_REQUEST_FAILED}.
 	 */
 	void onRequestFailed(final BluetoothDevice device, final int status);
 }
