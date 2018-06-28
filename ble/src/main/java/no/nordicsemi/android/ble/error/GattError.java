5533c885ad494e6a50db92bfdb6f0d55c6d568fe
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-06-28 13:39:40 +0200

    GATT errors as constants

--- ble/src/main/java/no/nordicsemi/android/ble/error/GattError.java
+++ ble/src/main/java/no/nordicsemi/android/ble/error/GattError.java
@@ -24,12 +24,13 @@ package no.nordicsemi.android.ble.error;
 import android.bluetooth.BluetoothGatt;
 
 /**
- * Parses the error numbers according to the <b>gatt_api.h</b> file from bluedroid stack.
- * See: https://android.googlesource.com/platform/external/bluetooth/bluedroid/+/android-5.1.0_r1/stack/include/gatt_api.h (and other versions) for details.
- * See also: https://android.googlesource.com/platform/external/libnfc-nci/+/master/src/include/hcidefs.h#447 for other possible HCI errors.
+ * Parses the GATT and HCI errors to human readable strings.
+ * <p>
+ * See: <a href="https://android.googlesource.com/platform/external/bluetooth/bluedroid/+/master/stack/include/gatt_api.h">gatt_api.h</a> for details.<br>
+ * See also: <a href="https://android.googlesource.com/platform/external/libnfc-nci/+/master/src/include/hcidefs.h#447">hcidefs.h</a> for other possible HCI errors.
  */
+@SuppressWarnings("WeakerAccess")
 public class GattError {
-
 	public static final int GATT_SUCCESS = BluetoothGatt.GATT_SUCCESS;
 	public static final int GATT_CONN_L2C_FAILURE = 0x01;
 	public static final int GATT_CONN_TIMEOUT = 0x08;
@@ -39,6 +40,45 @@ public class GattError {
 	public static final int GATT_CONN_LMP_TIMEOUT = 0x22;
 	public static final int GATT_CONN_CANCEL = 0x0100;
 	public static final int GATT_ERROR = 0x0085; // Device not reachable
+
+	public static final int GATT_INVALID_HANDLE = 0x0001;
+	public static final int GATT_READ_NOT_PERMIT = 0x0002;
+	public static final int GATT_WRITE_NOT_PERMIT = 0x0003;
+	public static final int GATT_INVALID_PDU = 0x0004;
+	public static final int GATT_INSUF_AUTHENTICATION = 0x0005;
+	public static final int GATT_REQ_NOT_SUPPORTED = 0x0006;
+	public static final int GATT_INVALID_OFFSET = 0x0007;
+	public static final int GATT_INSUF_AUTHORIZATION = 0x0008;
+	public static final int GATT_PREPARE_Q_FULL = 0x0009;
+	public static final int GATT_NOT_FOUND = 0x000a;
+	public static final int GATT_NOT_LONG = 0x000b;
+	public static final int GATT_INSUF_KEY_SIZE = 0x000c;
+	public static final int GATT_INVALID_ATTR_LEN = 0x000d;
+	public static final int GATT_ERR_UNLIKELY = 0x000e;
+	public static final int GATT_INSUF_ENCRYPTION = 0x000f;
+	public static final int GATT_UNSUPPORT_GRP_TYPE = 0x0010;
+	public static final int GATT_INSUF_RESOURCE = 0x0011;
+	public static final int GATT_CONTROLLER_BUSY = 0x003A;
+	public static final int GATT_UNACCEPT_CONN_INTERVAL = 0x003B;
+	public static final int GATT_ILLEGAL_PARAMETER = 0x0087;
+	public static final int GATT_NO_RESOURCES = 0x0080;
+	public static final int GATT_INTERNAL_ERROR = 0x0081;
+	public static final int GATT_WRONG_STATE = 0x0082;
+	public static final int GATT_DB_FULL = 0x0083;
+	public static final int GATT_BUSY = 0x0084;
+	public static final int GATT_CMD_STARTED = 0x0086;
+	public static final int GATT_PENDING = 0x0088;
+	public static final int GATT_AUTH_FAIL = 0x0089;
+	public static final int GATT_MORE = 0x008a;
+	public static final int GATT_INVALID_CFG = 0x008b;
+	public static final int GATT_SERVICE_STARTED = 0x008c;
+	public static final int GATT_ENCRYPTED_NO_MITM = 0x008d;
+	public static final int GATT_NOT_ENCRYPTED = 0x008e;
+	public static final int GATT_CONGESTED = 0x008f;
+	public static final int GATT_CCCD_CFG_ERROR = 0x00FD;
+	public static final int GATT_PROCEDURE_IN_PROGRESS = 0x00FE;
+	public static final int GATT_VALUE_OUT_OF_RANGE = 0x00FF;
+	public static final int TOO_MANY_OPEN_CONNECTIONS = 0x0101;
 	
 	/**
 	 * Converts the connection status given by the
@@ -74,7 +114,7 @@ public class GattError {
 	}
 
 	/**
-	 * Converts the bluetooth communication status given by other BluetoothGattCallbacks to error
+	 * Converts the Bluetooth communication status given by other BluetoothGattCallbacks to error
 	 * name. It also parses the DFU errors.
 	 *
 	 * @param error the status number.
@@ -82,85 +122,85 @@ public class GattError {
 	 */
 	public static String parse(final int error) {
 		switch (error) {
-			case 0x0001:
+			case GATT_INVALID_HANDLE:
 				return "GATT INVALID HANDLE";
-			case 0x0002:
+			case GATT_READ_NOT_PERMIT:
 				return "GATT READ NOT PERMIT";
-			case 0x0003:
+			case GATT_WRITE_NOT_PERMIT:
 				return "GATT WRITE NOT PERMIT";
-			case 0x0004:
+			case GATT_INVALID_PDU:
 				return "GATT INVALID PDU";
-			case 0x0005:
+			case GATT_INSUF_AUTHENTICATION:
 				return "GATT INSUF AUTHENTICATION";
-			case 0x0006:
+			case GATT_REQ_NOT_SUPPORTED:
 				return "GATT REQ NOT SUPPORTED";
-			case 0x0007:
+			case GATT_INVALID_OFFSET:
 				return "GATT INVALID OFFSET";
-			case 0x0008:
+			case GATT_INSUF_AUTHORIZATION:
 				return "GATT INSUF AUTHORIZATION";
-			case 0x0009:
+			case GATT_PREPARE_Q_FULL:
 				return "GATT PREPARE Q FULL";
-			case 0x000a:
+			case GATT_NOT_FOUND:
 				return "GATT NOT FOUND";
-			case 0x000b:
+			case GATT_NOT_LONG:
 				return "GATT NOT LONG";
-			case 0x000c:
+			case GATT_INSUF_KEY_SIZE:
 				return "GATT INSUF KEY SIZE";
-			case 0x000d:
+			case GATT_INVALID_ATTR_LEN:
 				return "GATT INVALID ATTR LEN";
-			case 0x000e:
+			case GATT_ERR_UNLIKELY:
 				return "GATT ERR UNLIKELY";
-			case 0x000f:
+			case GATT_INSUF_ENCRYPTION:
 				return "GATT INSUF ENCRYPTION";
-			case 0x0010:
+			case GATT_UNSUPPORT_GRP_TYPE:
 				return "GATT UNSUPPORT GRP TYPE";
-			case 0x0011:
+			case GATT_INSUF_RESOURCE:
 				return "GATT INSUF RESOURCE";
-			case 0x0022:
+			case GATT_CONN_LMP_TIMEOUT:
 				return "GATT CONN LMP TIMEOUT";
-			case 0x003A:
+			case GATT_CONTROLLER_BUSY:
 				return "GATT CONTROLLER BUSY";
-			case 0x003B:
+			case GATT_UNACCEPT_CONN_INTERVAL:
 				return "GATT UNACCEPT CONN INTERVAL";
-			case 0x0087:
+			case GATT_ILLEGAL_PARAMETER:
 				return "GATT ILLEGAL PARAMETER";
-			case 0x0080:
+			case GATT_NO_RESOURCES:
 				return "GATT NO RESOURCES";
-			case 0x0081:
+			case GATT_INTERNAL_ERROR:
 				return "GATT INTERNAL ERROR";
-			case 0x0082:
+			case GATT_WRONG_STATE:
 				return "GATT WRONG STATE";
-			case 0x0083:
+			case GATT_DB_FULL:
 				return "GATT DB FULL";
-			case 0x0084:
+			case GATT_BUSY:
 				return "GATT BUSY";
-			case 0x0085:
+			case GATT_ERROR:
 				return "GATT ERROR";
-			case 0x0086:
+			case GATT_CMD_STARTED:
 				return "GATT CMD STARTED";
-			case 0x0088:
+			case GATT_PENDING:
 				return "GATT PENDING";
-			case 0x0089:
+			case GATT_AUTH_FAIL:
 				return "GATT AUTH FAIL";
-			case 0x008a:
+			case GATT_MORE:
 				return "GATT MORE";
-			case 0x008b:
+			case GATT_INVALID_CFG:
 				return "GATT INVALID CFG";
-			case 0x008c:
+			case GATT_SERVICE_STARTED:
 				return "GATT SERVICE STARTED";
-			case 0x008d:
+			case GATT_ENCRYPTED_NO_MITM:
 				return "GATT ENCRYPTED NO MITM";
-			case 0x008e:
+			case GATT_NOT_ENCRYPTED:
 				return "GATT NOT ENCRYPTED";
-			case 0x008f:
+			case GATT_CONGESTED:
 				return "GATT CONGESTED";
-			case 0x00FD:
+			case GATT_CCCD_CFG_ERROR:
 				return "GATT CCCD CFG ERROR";
-			case 0x00FE:
+			case GATT_PROCEDURE_IN_PROGRESS:
 				return "GATT PROCEDURE IN PROGRESS";
-			case 0x00FF:
+			case GATT_VALUE_OUT_OF_RANGE:
 				return "GATT VALUE OUT OF RANGE";
-			case 0x0101:
+			case TOO_MANY_OPEN_CONNECTIONS:
 				return "TOO MANY OPEN CONNECTIONS";
 			default:
 				return "UNKNOWN (" + error + ")";
