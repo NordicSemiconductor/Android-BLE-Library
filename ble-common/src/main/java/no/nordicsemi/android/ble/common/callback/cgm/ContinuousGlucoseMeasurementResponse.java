b2135005816ceda01ef62a766bf0e3bd96fc8c7f
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-02-16 14:32:59 +0100

    Fix issue when only one record is returned in CGMS profile.
    ContinuousGlucoseMeasurementResponse class on..Received() method
    can be invoked multiple times but the class can only store a single
    record. As a result old values were lost. To save all the values
    the class has been modified to use ArrayList as a values storage.

--- ble-common/src/main/java/no/nordicsemi/android/ble/common/callback/cgm/ContinuousGlucoseMeasurementResponse.java
+++ ble-common/src/main/java/no/nordicsemi/android/ble/common/callback/cgm/ContinuousGlucoseMeasurementResponse.java
@@ -28,6 +28,11 @@ import android.os.Parcelable;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 import no.nordicsemi.android.ble.data.Data;
 import no.nordicsemi.android.ble.exception.InvalidDataException;
 import no.nordicsemi.android.ble.exception.RequestFailedException;
@@ -53,14 +58,7 @@ import no.nordicsemi.android.ble.exception.RequestFailedException;
  */
 @SuppressWarnings("unused")
 public final class ContinuousGlucoseMeasurementResponse extends ContinuousGlucoseMeasurementDataCallback implements CRCSecuredResponse, Parcelable {
-	private float glucoseConcentration;
-	@Nullable
-	private Float trend;
-	@Nullable
-	private Float quality;
-	@Nullable
-	private CGMStatus status;
-	private int timeOffset;
+	private ArrayList<ContinuousGlucoseMeasurementResponseItem> items = new ArrayList<>();
 	private boolean secured;
 	private boolean crcValid;
 
@@ -72,13 +70,16 @@ public final class ContinuousGlucoseMeasurementResponse extends ContinuousGlucos
 	public void onContinuousGlucoseMeasurementReceived(@NonNull final BluetoothDevice device, final float glucoseConcentration,
 													   @Nullable final Float cgmTrend, @Nullable final Float cgmQuality,
 													   @Nullable final CGMStatus status, final int timeOffset, final boolean secured) {
-		this.glucoseConcentration = glucoseConcentration;
-		this.trend = cgmTrend;
-		this.quality = cgmQuality;
-		this.status = status;
-		this.timeOffset = timeOffset;
+
+		ContinuousGlucoseMeasurementResponseItem item = new ContinuousGlucoseMeasurementResponseItem(
+				glucoseConcentration,
+				cgmTrend,
+				cgmQuality,
+				status,
+				timeOffset
+		);
+		items.add(item);
 		this.secured = secured;
-		this.crcValid = secured;
 	}
 
 	@Override
@@ -88,62 +89,11 @@ public final class ContinuousGlucoseMeasurementResponse extends ContinuousGlucos
 		this.crcValid = false;
 	}
 
-	public float getGlucoseConcentration() {
-		return glucoseConcentration;
-	}
-
-	@Nullable
-	public Float getTrend() {
-		return trend;
-	}
-
-	@Nullable
-	public Float getQuality() {
-		return quality;
-	}
-
-	@Nullable
-	public CGMStatus getStatus() {
-		return status;
-	}
-
-	public int getTimeOffset() {
-		return timeOffset;
-	}
-
-	@Override
-	public boolean isSecured() {
-		return secured;
-	}
-
-	@Override
-	public boolean isCrcValid() {
-		return crcValid;
-	}
-
 	// Parcelable
 	private ContinuousGlucoseMeasurementResponse(final Parcel in) {
 		super(in);
-		glucoseConcentration = in.readFloat();
-		if (in.readByte() == 0) {
-			trend = null;
-		} else {
-			trend = in.readFloat();
-		}
-		if (in.readByte() == 0) {
-			quality = null;
-		} else {
-			quality = in.readFloat();
-		}
-		if (in.readByte() == 0) {
-			status = null;
-		} else {
-			final int warningStatus = in.readInt();
-			final int calibrationTempStatus = in.readInt();
-			final int sensorStatus = in.readInt();
-			status = new CGMStatus(warningStatus, calibrationTempStatus, sensorStatus);
-		}
-		timeOffset = in.readInt();
+
+		in.readList(items, ContinuousGlucoseMeasurementResponseItem.class.getClassLoader());
 		secured = in.readByte() != 0;
 		crcValid = in.readByte() != 0;
 	}
@@ -151,28 +101,8 @@ public final class ContinuousGlucoseMeasurementResponse extends ContinuousGlucos
 	@Override
 	public void writeToParcel(final Parcel dest, final int flags) {
 		super.writeToParcel(dest, flags);
-		dest.writeFloat(glucoseConcentration);
-		if (trend == null) {
-			dest.writeByte((byte) 0);
-		} else {
-			dest.writeByte((byte) 1);
-			dest.writeFloat(trend);
-		}
-		if (quality == null) {
-			dest.writeByte((byte) 0);
-		} else {
-			dest.writeByte((byte) 1);
-			dest.writeFloat(quality);
-		}
-		if (status == null) {
-			dest.writeByte((byte) 0);
-		} else {
-			dest.writeByte((byte) 1);
-			dest.writeInt(status.warningStatus);
-			dest.writeInt(status.calibrationTempStatus);
-			dest.writeInt(status.sensorStatus);
-		}
-		dest.writeInt(timeOffset);
+
+		dest.writeList(items);
 		dest.writeByte((byte) (secured ? 1 : 0));
 		dest.writeByte((byte) (crcValid ? 1 : 0));
 	}
@@ -188,4 +118,18 @@ public final class ContinuousGlucoseMeasurementResponse extends ContinuousGlucos
 			return new ContinuousGlucoseMeasurementResponse[size];
 		}
 	};
+
+	public ArrayList<ContinuousGlucoseMeasurementResponseItem> getItems() {
+		return items;
+	}
+
+	@Override
+	public boolean isSecured() {
+		return secured;
+	}
+
+	@Override
+	public boolean isCrcValid() {
+		return crcValid;
+	}
 }
---
+++ ble-common/src/main/java/no/nordicsemi/android/ble/common/callback/cgm/ContinuousGlucoseMeasurementResponseItem.java
@@ -0,0 +1,141 @@
+package no.nordicsemi.android.ble.common.callback.cgm;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import androidx.annotation.Nullable;
+import no.nordicsemi.android.ble.common.profile.cgm.CGMTypes;
+
+public class ContinuousGlucoseMeasurementResponseItem implements Parcelable {
+    private float glucoseConcentration;
+    @Nullable
+    private Float trend;
+    @Nullable
+    private Float quality;
+    @Nullable
+    private CGMTypes.CGMStatus status;
+    private int timeOffset;
+
+    public ContinuousGlucoseMeasurementResponseItem(
+            float glucoseConcentration,
+            @Nullable Float trend,
+            @Nullable Float quality,
+            @Nullable CGMTypes.CGMStatus status,
+            int timeOffset
+    ) {
+        this.glucoseConcentration = glucoseConcentration;
+        this.trend = trend;
+        this.quality = quality;
+        this.status = status;
+        this.timeOffset = timeOffset;
+    }
+
+
+    public float getGlucoseConcentration() {
+        return glucoseConcentration;
+    }
+
+    @Nullable
+    public Float getTrend() {
+        return trend;
+    }
+
+    @Nullable
+    public Float getQuality() {
+        return quality;
+    }
+
+    @Nullable
+    public CGMTypes.CGMStatus getStatus() {
+        return status;
+    }
+
+    public int getTimeOffset() {
+        return timeOffset;
+    }
+
+    public void setGlucoseConcentration(float glucoseConcentration) {
+        this.glucoseConcentration = glucoseConcentration;
+    }
+
+    public void setTrend(Float trend) {
+        this.trend = trend;
+    }
+
+    public void setQuality(Float quality) {
+        this.quality = quality;
+    }
+
+    public void setStatus(CGMTypes.CGMStatus status) {
+        this.status = status;
+    }
+
+    public void setTimeOffset(int timeOffset) {
+        this.timeOffset = timeOffset;
+    }
+
+    ContinuousGlucoseMeasurementResponseItem(final Parcel in) {
+        glucoseConcentration = in.readFloat();
+        if (in.readByte() == 0) {
+            trend = null;
+        } else {
+            trend = in.readFloat();
+        }
+        if (in.readByte() == 0) {
+            quality = null;
+        } else {
+            quality = in.readFloat();
+        }
+        if (in.readByte() == 0) {
+            status = null;
+        } else {
+            final int warningStatus = in.readInt();
+            final int calibrationTempStatus = in.readInt();
+            final int sensorStatus = in.readInt();
+            status = new CGMTypes.CGMStatus(warningStatus, calibrationTempStatus, sensorStatus);
+        }
+        timeOffset = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeFloat(glucoseConcentration);
+        if (trend == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeFloat(trend);
+        }
+        if (quality == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeFloat(quality);
+        }
+        if (status == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeInt(status.warningStatus);
+            dest.writeInt(status.calibrationTempStatus);
+            dest.writeInt(status.sensorStatus);
+        }
+        dest.writeInt(timeOffset);
+    }
+
+    public static final Creator<ContinuousGlucoseMeasurementResponseItem> CREATOR = new Creator<ContinuousGlucoseMeasurementResponseItem>() {
+        @Override
+        public ContinuousGlucoseMeasurementResponseItem createFromParcel(final Parcel in) {
+            return new ContinuousGlucoseMeasurementResponseItem(in);
+        }
+
+        @Override
+        public ContinuousGlucoseMeasurementResponseItem[] newArray(final int size) {
+            return new ContinuousGlucoseMeasurementResponseItem[size];
+        }
+    };
+}
