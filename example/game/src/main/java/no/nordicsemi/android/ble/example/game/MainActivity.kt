e7c7af9ce7202fa86ba0ed1678dfafef1a0321c4
hiar <aryal.himali@gmail.com>
2022-10-05 15:43:18 +0200

    defined navigation destinations

--- example/game/src/main/java/no/nordicsemi/android/ble/example/game/MainActivity.kt
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/MainActivity.kt
@@ -2,23 +2,13 @@ package no.nordicsemi.android.ble.example.game
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import no.nordicsemi.android.ble.example.game.navigation.AdvertisingDestinations
-import no.nordicsemi.android.ble.example.game.server.viewmodel.AdvertisingViewModel
+import no.nordicsemi.android.ble.example.game.navigation.ClientDestinations
+import no.nordicsemi.android.ble.example.game.navigation.HomeScreen
+import no.nordicsemi.android.ble.example.game.navigation.ServerDestinations
 import no.nordicsemi.android.common.navigation.DestinationId
-import no.nordicsemi.android.common.navigation.NavigationManager
 import no.nordicsemi.android.common.navigation.NavigationView
-import no.nordicsemi.android.common.permission.RequireBluetooth
 import no.nordicsemi.android.common.theme.NordicActivity
 import no.nordicsemi.android.common.theme.NordicTheme
 
@@ -30,39 +20,20 @@ class MainActivity : NordicActivity() {
         setContent {
             NordicTheme {
                 Surface {
-                    RequireBluetooth {
-                        NavigationView(destinations = AdvertisingDestinations)
-
-                    }
+                    NavigationView(
+                        destinations = HomeScreen
+                                + ServerDestinations
+                                + ClientDestinations
+                    )
                 }
             }
         }
     }
+
     companion object {
         val Main = DestinationId(NavigationConst.HOME)
-    }
-}
-@Preview
-@Composable
-fun AdvertisingScreen(
-    navigationManager: NavigationManager? = null
-) {
-    val advertisingViewModel: AdvertisingViewModel = hiltViewModel()
-    Column(
-        modifier = Modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Starting Advertising...")
-        Spacer(modifier = Modifier.padding(16.dp))
-        Box(
-            modifier = Modifier.clickable {
-                advertisingViewModel.startAdvertising()
-                                          },
-            contentAlignment = Alignment.Center
-            ) {
-            Text(text = "Start Advertising")
-        }
+        val Server = DestinationId(NavigationConst.SERVER)
+        val Client = DestinationId(NavigationConst.CLIENT)
     }
 }
 
--- example/game/src/main/java/no/nordicsemi/android/ble/example/game/NavigationConst.kt
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/NavigationConst.kt
@@ -2,4 +2,6 @@ package no.nordicsemi.android.ble.example.game
 
 object NavigationConst {
     const val HOME = "home"
+    const val SERVER = "server"
+    const val CLIENT = "client"
 }
