003c5811581bd63d49e7abce3d717ee7e5aede86
hiar <aryal.himali@gmail.com>
2022-10-05 15:40:20 +0200

    Bluetooth adapter

---
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/di/BluetoothAdapterModule.kt
@@ -0,0 +1,24 @@
+package no.nordicsemi.android.ble.example.game.di
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object BluetoothAdapterModule {
+
+    @Provides
+    fun provideBluetoothAdapter(
+        @ApplicationContext context: Context,
+    ): BluetoothAdapter {
+        val manager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        return manager.adapter
+    }
+
+}
