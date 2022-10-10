771292a47f85aa021977c6679fe8436fb646971d
hiar <aryal.himali@gmail.com>
2022-10-10 10:39:08 +0200

    changed ViewModelComponent to SingletonComponent

--- example/game/src/main/java/no/nordicsemi/android/ble/example/game/di/BluetoothAdapterModule.kt
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/di/BluetoothAdapterModule.kt
@@ -6,11 +6,11 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 object BluetoothAdapterModule {
 
     @Provides
