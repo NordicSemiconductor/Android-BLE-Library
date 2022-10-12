0b269cfeea12209ae8255063103ad4dc2d62dc1b
hiar <aryal.himali@gmail.com>
2022-10-12 15:26:49 +0200

    read response and question proto

--- example/game/src/main/java/no/nordicsemi/android/ble/example/game/client/data/QuestionRequest.kt
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/client/data/QuestionRequest.kt
@@ -3,17 +3,40 @@ package no.nordicsemi.android.ble.example.game.client.data
 import android.bluetooth.BluetoothDevice
 import android.util.Log
 import no.nordicsemi.android.ble.data.Data
+import no.nordicsemi.android.ble.example.game.proto.OpCodeProto
 import no.nordicsemi.android.ble.example.game.proto.RequestProto
 import no.nordicsemi.android.ble.example.game.quiz.repository.Question
 import no.nordicsemi.android.ble.example.game.quiz.repository.toQuestion
 import no.nordicsemi.android.ble.response.ReadResponse
 
-class QuestionRequest: ReadResponse() {
+class Request: ReadResponse() {
+    enum class Type {
+        QUESTION,
+        ANSWER,
+        GAME_OVER,
+    }
+    var type: Type? = null
+    var answerId: Int? = null
     var question: Question? = null
+
 
     override fun onDataReceived(device: BluetoothDevice, data: Data) {
         val bytes = data.value!!
-        question = RequestProto.ADAPTER.decode(bytes).question?.toQuestion()
-        Log.d("QuestionRequest", "onDataReceived: ${question?.question} ")
+        val request = RequestProto.ADAPTER.decode(bytes)
+        when (request.opCode) {
+            OpCodeProto.NEW_QUESTION -> {
+                type = Type.QUESTION
+                question = request.question?.toQuestion()
+                Log.d("QuestionRequest", "onDataReceived: ${question?.question} ")
+            }
+            OpCodeProto.RESULT -> {
+                type = Type.ANSWER
+                answerId = request.answerId
+                Log.d("QuestionRequest", "onDataReceived: $answerId ")
+            }
+            else -> {}
+            // game over
+        }
+
     }
 }
