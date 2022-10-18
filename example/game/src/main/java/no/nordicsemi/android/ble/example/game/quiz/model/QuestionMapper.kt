26e02a0c44e8e7b886ec6950b4dfc91102c792ac
hiar <aryal.himali@gmail.com>
2022-10-18 15:31:27 +0200

    shortened the random ids

--- example/game/src/main/java/no/nordicsemi/android/ble/example/game/quiz/model/QuestionMapper.kt
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/quiz/model/QuestionMapper.kt
@@ -4,19 +4,19 @@ import no.nordicsemi.android.ble.example.game.quiz.repository.Answer
 import no.nordicsemi.android.ble.example.game.quiz.repository.Question
 import no.nordicsemi.android.ble.example.game.quiz.repository.Questions
 import no.nordicsemi.android.ble.example.game.quiz.repository.QuestionsRemote
-import kotlin.random.Random
+
 /**
  * A mapper object to map the remote data with the Local data format.
  */
-
 object QuestionMapper {
     fun mapRemoteToLocal(questionRemote: QuestionsRemote): Questions {
         val results = questionRemote.results.map { remoteQuestion ->
+            val ids = (0..remoteQuestion.incorrect_answers.size + 1).toList().shuffled()
             val answers = mutableListOf<Answer>()
-            val correctId = Random.nextInt()
+            val correctId = ids[0]
             answers.add(Answer(remoteQuestion.correct_answer, correctId))
-            answers.addAll(remoteQuestion.incorrect_answers.map {
-                Answer(it, Random.nextInt())
+            answers.addAll(remoteQuestion.incorrect_answers.mapIndexed { index, answer ->
+                Answer(answer, ids[index + 1])
             })
             answers.shuffle()
             Question(remoteQuestion.question, answers, correctId)
