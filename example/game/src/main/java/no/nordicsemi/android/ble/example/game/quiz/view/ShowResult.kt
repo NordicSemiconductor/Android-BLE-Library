8d0b6ef526ec23947dd7884a773aab305d7bf406
hiar <aryal.himali@gmail.com>
2022-11-02 10:51:07 +0100

    minor changes

--- example/game/src/main/java/no/nordicsemi/android/ble/example/game/quiz/view/ShowResult.kt
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/quiz/view/ShowResult.kt
@@ -1,59 +1,56 @@
 package no.nordicsemi.android.ble.example.game.quiz.view
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import no.nordicsemi.android.ble.example.game.R
-import no.nordicsemi.android.ble.example.game.server.data.FinalResult
+import no.nordicsemi.android.ble.example.game.server.data.Result
+import no.nordicsemi.android.common.theme.NordicTheme
 
 @Composable
-fun ShowResult(result: List<FinalResult>) {
-    val sortedResult: List<FinalResult> = result.sortedByDescending { it.score }
-    val openDialog = remember { mutableStateOf(true) }
-    if (openDialog.value) {
-        AlertDialog(
-            onDismissRequest = { },
-            title = {
-                Text(text = stringResource(id = R.string.game_over))
-            },
-            text = {
-                Text(text = stringResource(id = R.string.show_result_des))
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
-                    }
-                ) {
-                    Text(text = stringResource(id = R.string.show_result))
-                }
-            },
-        )
+fun ShowResultView(result: List<Result?>) {
+    val sortedResult: List<Result?> = result.sortedByDescending { it?.score }
+    var openDialog by remember { mutableStateOf(true) }
+    if (openDialog) {
+        ShowResultDialog(
+            onDismiss = { openDialog = false }) {
+            openDialog = false
+        }
     } else {
+        Row(
+            modifier = Modifier.padding(start = 24.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Text(
+                text = stringResource(id = R.string.winner),
+            )
+            Text(
+                text = stringResource(id = R.string.score),
+            )
+        }
         LazyColumn(
             modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(16.dp),
+            contentPadding = PaddingValues(24.dp),
         ) {
             items(items = sortedResult) { sortedResult ->
                 Row {
+                    sortedResult?.name?.let {
+                        Text(
+                            text = it,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
                     Text(
-                        text = sortedResult.name,
-                        modifier = Modifier.padding(16.dp)
-                    )
-                    Text(
-                        text = sortedResult.score.toString(),
-                        modifier = Modifier.padding(16.dp)
+                        text = sortedResult?.score.toString(),
+                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
@@ -61,36 +58,42 @@ fun ShowResult(result: List<FinalResult>) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowResultDialog(
+    onDismiss: () -> Unit,
+    onClick: () -> Unit,
+) {
+    AlertDialog(
+        properties = DialogProperties(
+            dismissOnClickOutside = false
+        ),
+        onDismissRequest = onDismiss,
+        title = {
+            Text(
+                text = stringResource(id = R.string.game_over),
+                textAlign = TextAlign.Center
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onClick
+            ) {
+                Text(text = stringResource(id = R.string.show_result))
+            }
+        },
+    )
+}
+
 @Preview
 @Composable
-fun GameOverPopUpDialog() {
-    val openDialog = remember { mutableStateOf(true) }
-    var clientName by remember { mutableStateOf(TextFieldValue("")) }
-
-    if (openDialog.value) {
-        AlertDialog(
-            onDismissRequest = { },
-            title = {
-                Text(
-                    text = "Enter player's name",
-                    textAlign = TextAlign.Center
-                )
-            },
-            text = {
-                TextField(value = clientName,
-                    onValueChange = { clientName = it }
-                )
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
-                    }
-                ) {
-                    Text("Send")
-                }
-            },
+fun ShowResultDialog_Preview() {
+    NordicTheme {
+        ShowResultView(
+            result = listOf(
+                Result("User 1", 1),
+                Result("User 2", 5),
+                Result("User 3", 4)
+            )
         )
     }
 }
