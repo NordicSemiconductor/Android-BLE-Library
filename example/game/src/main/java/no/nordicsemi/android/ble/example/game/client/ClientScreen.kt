1fa1b35332e11f4e2d76c114aee37b9fbb3d4c57
hiar <aryal.himali@gmail.com>
2022-11-02 09:37:59 +0100

    separated into different views

---
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/client/ClientScreen.kt
@@ -0,0 +1,103 @@
+package no.nordicsemi.android.ble.example.game.client
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import no.nordicsemi.android.ble.example.game.R
+import no.nordicsemi.android.ble.example.game.client.view.*
+import no.nordicsemi.android.ble.example.game.client.viewmodel.ClientViewModel
+import no.nordicsemi.android.ble.example.game.quiz.view.PlayersNameDialog
+import no.nordicsemi.android.ble.example.game.quiz.view.QuestionContentView
+import no.nordicsemi.android.ble.example.game.quiz.view.ShowResultView
+import no.nordicsemi.android.ble.ktx.state.ConnectionState
+import no.nordicsemi.android.common.navigation.NavigationManager
+import no.nordicsemi.android.common.permission.RequireBluetooth
+import no.nordicsemi.android.common.theme.view.NordicAppBar
+
+@Composable
+fun ClientScreen(
+    navigationManager: NavigationManager,
+) {
+    Column {
+        NordicAppBar(
+            text = stringResource(id = R.string.client),
+            onNavigationButtonClick = {
+                navigationManager.navigateUp()
+            }
+        )
+
+        RequireBluetooth {
+            val clientViewModel: ClientViewModel = hiltViewModel()
+            val state by clientViewModel.state.collectAsState()
+
+            when (state) {
+                ConnectionState.Initializing -> {
+                    InitializingView()
+                }
+                ConnectionState.Connecting -> {
+                    ConnectingView()
+                }
+                is ConnectionState.Disconnected -> {
+                    DisconnectedView()
+                }
+                ConnectionState.Ready -> {
+                    val question by clientViewModel.question.collectAsState()
+                    val results by clientViewModel.result.collectAsState()
+
+                    question?.let { q ->
+                        val correctAnswerId by clientViewModel.answer.collectAsState()
+                        val selectedAnswerId by clientViewModel.selectedAnswer
+                        val ticks by clientViewModel.ticks.collectAsState()
+
+                        when (results?.isGameOver) {
+                            true -> results?.result?.let { result -> ShowResultView(result = result) }
+                            else -> {
+                                QuestionContentView(
+                                    question = q,
+                                    correctAnswerId = correctAnswerId,
+                                    selectedAnswerId = selectedAnswerId,
+                                    ticks = ticks,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) { answerChosen ->
+                                    clientViewModel.sendAnswer(answerChosen)
+                                }
+                            }
+                        }
+                    } ?: run {
+                        var openDialog by remember { mutableStateOf(true) }
+                        var playersName by remember { mutableStateOf("") }
+                        var isDuplicate by remember { mutableStateOf(false) }
+
+                        if (openDialog) {
+                            PlayersNameDialog(
+                                playersName = playersName,
+                                isDuplicate = isDuplicate,
+                                onDismiss = { openDialog = false },
+                                onNameSet = {
+                                    playersName = it
+                                    isDuplicate = false
+                                }) {
+                                if (playersName.isNotEmpty()) {
+                                    results?.result?.find { it.name == playersName }
+                                        ?.let {
+                                            isDuplicate = true
+                                        }
+                                        ?: run {
+                                            clientViewModel.sendName(playersName)
+                                            openDialog = false
+                                        }
+                                }
+                            }
+                        } else ConnectedView()
+                    }
+                }
+                else -> LoadingView()
+            }
+        }
+    }
+}
+
+
---
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/client/view/ConnectedView.kt
@@ -0,0 +1,33 @@
+package no.nordicsemi.android.ble.example.game.client.view
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import no.nordicsemi.android.ble.example.game.R
+
+
+@Composable
+fun ConnectedView() {
+    OutlinedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp),
+    ) {
+        Text(
+            text = stringResource(id = R.string.connected),
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ConnectedView_Preview(modifier: Modifier = Modifier){
+    ConnectedView()
+}
