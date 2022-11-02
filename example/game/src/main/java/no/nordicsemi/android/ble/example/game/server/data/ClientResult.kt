c9299b2b7c3ee1c45d3fa8d33e785d4afd50540a
hiar <aryal.himali@gmail.com>
2022-11-02 11:26:35 +0100

    minor changes

--- example/game/src/main/java/no/nordicsemi/android/ble/example/game/server/data/ClientResult.kt
+++ example/game/src/main/java/no/nordicsemi/android/ble/example/game/server/data/ClientResult.kt
@@ -3,10 +3,9 @@ package no.nordicsemi.android.ble.example.game.server.data
 import no.nordicsemi.android.ble.example.game.proto.ResultProto
 
 /**
- * Result to be received from remote players
- * @property playersName Name of the player
- * @property selectedAnswerId Selected answer id
- *
+ * Result to be received from remote players.
+ * @property playersName Name of the player.
+ * @property selectedAnswerId Selected answer id.
  * */
 data class ClientResult(
     val playersName: String,
