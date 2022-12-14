3778807b7feb19322e49a94be0a31c65f7809eef
hiar <aryal.himali@gmail.com>
2022-12-14 12:13:18 +0100

    file rename

--- test/src/main/java/no/nordicsemi/andorid/ble/test/Destinations.kt
+++ test/src/main/java/no/nordicsemi/andorid/ble/test/Destinations.kt
@@ -3,15 +3,15 @@ package no.nordicsemi.andorid.ble.test
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.hilt.navigation.compose.hiltViewModel
-import no.nordicsemi.andorid.ble.test.client.view.ScanningScreen
-import no.nordicsemi.andorid.ble.test.server.view.AdvertisingScreen
+import no.nordicsemi.andorid.ble.test.client.view.ClientScreen
+import no.nordicsemi.andorid.ble.test.server.view.ServerScreen
 import no.nordicsemi.android.common.navigation.createSimpleDestination
 import no.nordicsemi.android.common.navigation.defineDestination
 import no.nordicsemi.android.common.navigation.viewmodel.SimpleNavigationViewModel
 
 val StartDestination = createSimpleDestination("start")
-val ClientDestination = createSimpleDestination("scan-destination")
-val ServerDestination = createSimpleDestination("advertise-destination")
+val ClientDestination = createSimpleDestination("client-destination")
+val ServerDestination = createSimpleDestination("server-destination")
 
 @RequiresApi(Build.VERSION_CODES.O)
 val Destinations = listOf(
@@ -19,8 +19,8 @@ val Destinations = listOf(
         val viewModel: SimpleNavigationViewModel = hiltViewModel()
         StartScreen(onNavigation = { viewModel.navigateTo(it) })
     },
-    defineDestination(ClientDestination) { ScanningScreen() },
-    defineDestination(ServerDestination) { AdvertisingScreen() }
+    defineDestination(ClientDestination) { ClientScreen() },
+    defineDestination(ServerDestination) { ServerScreen() }
 )
 
 
