5ffc880714a1b45998d5786616fa21795349469e
hiar <aryal.himali@gmail.com>
2022-12-19 14:45:40 +0100

    changed loading view

--- test/src/main/java/no/nordicsemi/andorid/ble/test/server/view/LoadingView.kt
+++ test/src/main/java/no/nordicsemi/andorid/ble/test/server/view/LoadingView.kt
@@ -1,26 +1,24 @@
 package no.nordicsemi.andorid.ble.test.server.view
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import no.nordicsemi.andorid.ble.test.R
 import no.nordicsemi.android.common.theme.NordicTheme
 
 @Composable
 fun LoadingView() {
-    OutlinedCard(
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(32.dp),
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = stringResource(id = R.string.loading),
+        CircularProgressIndicator(
             modifier = Modifier.padding(16.dp)
         )
     }
