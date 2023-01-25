d79f4155f0b5cf6cd9ebb47ce67bbee7af9fa8ec
hiar <himali.aryal@nordicsemi.no>
2023-01-25 15:36:33 +0100

    added reliable write

--- test/src/main/java/no/nordicsemi/andorid/ble/test/client/tests/TestReliableWrite.kt
+++ test/src/main/java/no/nordicsemi/andorid/ble/test/client/tests/TestReliableWrite.kt
@@ -2,17 +2,17 @@ package no.nordicsemi.andorid.ble.test.client.tests
 
 import no.nordicsemi.andorid.ble.test.client.data.RELIABLE_WRITE
 import no.nordicsemi.andorid.ble.test.client.data.reliableRequest
+import no.nordicsemi.andorid.ble.test.client.data.secondReliableRequest
 import no.nordicsemi.andorid.ble.test.client.repository.ClientConnection
 import no.nordicsemi.andorid.ble.test.client.task.TaskManager
 import no.nordicsemi.andorid.ble.test.server.data.TestCase
-import no.nordicsemi.android.ble.ktx.suspend
 
 class TestReliableWrite : TaskManager {
     // Start the task
     override suspend fun start(
         clientConnection: ClientConnection
     ) {
-        clientConnection.testReliableWrite(reliableRequest).suspend()
+        clientConnection.testReliableWrite(listOf( reliableRequest, secondReliableRequest))
     }
 
     // Handle task completion
