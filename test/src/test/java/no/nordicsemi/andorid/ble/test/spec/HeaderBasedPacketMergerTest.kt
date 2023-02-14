b165a8ad518c8322516212eb75633cd6aa5eeb7c
hiar <himali.aryal@nordicsemi.no>
2023-02-14 10:45:47 +0100

    added test cases

--- test/src/test/java/no/nordicsemi/andorid/ble/test/spec/HeaderBasedPacketMergerTest.kt
+++ test/src/test/java/no/nordicsemi/andorid/ble/test/spec/HeaderBasedPacketMergerTest.kt
@@ -5,18 +5,24 @@ import org.junit.Assert.assertArrayEquals
 import org.junit.Test
 
 class HeaderBasedPacketMergerTest {
-    @Test
-    fun testMerge() {
-        val firstPacket = byteArrayOf(0, 5, 1, 2, 3)
-        val lastPacket = byteArrayOf(4, 5)
-        val outputs: DataStream = object : DataStream() {}
-        val expectedResult = byteArrayOf(1, 2, 3, 4, 5)
+    private val firstPacket = byteArrayOf(0, 5, 1, 2, 3)
+    private val lastPacket = byteArrayOf(4, 5)
+    private val fullPacket = byteArrayOf(0, 5, 1, 2, 3, 4, 5)
+    private val outputs: DataStream = object : DataStream() {}
+    private val headerBasedPacketMerger = HeaderBasedPacketMerger()
+    private val expectedResult = byteArrayOf(1, 2, 3, 4, 5)
 
-        val headerBasedPacketMerger = HeaderBasedPacketMerger()
+    @Test
+    fun testLargerMessage() {
         headerBasedPacketMerger.merge(outputs, firstPacket, 0)
         headerBasedPacketMerger.merge(outputs, lastPacket, 1)
 
         assertArrayEquals(expectedResult, outputs.toByteArray())
     }
 
+    @Test
+    fun testFullMessage() {
+        headerBasedPacketMerger.merge(outputs, fullPacket, 0)
+        assertArrayEquals(expectedResult, outputs.toByteArray())
+    }
 }
