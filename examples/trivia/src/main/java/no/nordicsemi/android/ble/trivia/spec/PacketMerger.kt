6e7964e25fc8f3de08c64ff1d2385a4850aae77b
hiar <himali.aryal@nordicsemi.no>
2023-02-15 10:00:48 +0100

    bug fix

--- examples/trivia/src/main/java/no/nordicsemi/android/ble/trivia/spec/PacketMerger.kt
+++ examples/trivia/src/main/java/no/nordicsemi/android/ble/trivia/spec/PacketMerger.kt
@@ -4,8 +4,9 @@ import no.nordicsemi.android.ble.data.DataMerger
 import no.nordicsemi.android.ble.data.DataStream
 import java.nio.ByteBuffer
 
-class PacketMerger: DataMerger {
-    var expectedSize = 0
+class PacketMerger : DataMerger {
+    private var expectedSize = 0
+    private var receivedDataSize = 0
 
     /**
      * A method that merges the last packet into the output message. All bytes from the lastPacket
@@ -14,18 +15,20 @@ class PacketMerger: DataMerger {
      * @param output     the stream for the output message, initially empty.
      * @param lastPacket the data received in the last read/notify/indicate operation.
      * @param index      an index of the packet, 0-based.
-     * @return True, if the message is complete, false if more data are expected.
+     * @return True,    if the message is complete, false if more data are expected.
      */
     override fun merge(output: DataStream, lastPacket: ByteArray?, index: Int): Boolean {
         if (lastPacket == null)
             return false
 
         val buffer = ByteBuffer.wrap(lastPacket)
+        receivedDataSize += buffer.remaining()
+
         if (index == 0) {
             expectedSize = buffer.short.toInt()
         }
 
-        if (buffer.remaining() == expectedSize) {
+        if ((receivedDataSize - 2) == expectedSize) {
             ByteArray(expectedSize)
                 .apply { buffer.get(this) }
                 .also { output.write(it) }
