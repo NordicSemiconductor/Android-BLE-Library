cc5ac391308b6edd4ca767b6f41033505c475b72
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2025-03-04 14:26:19 +0100

    Error doc links

--- ble/src/main/java/no/nordicsemi/android/ble/error/GattError.java
+++ ble/src/main/java/no/nordicsemi/android/ble/error/GattError.java
@@ -42,19 +42,27 @@ public class GattError {
 	public static final int GATT_ERROR = 0x0085; // Device not reachable
 
 	public static final int GATT_INVALID_HANDLE = 0x0001;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_READ_NOT_PERMITTED
 	public static final int GATT_READ_NOT_PERMIT = 0x0002;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_WRITE_NOT_PERMITTED
 	public static final int GATT_WRITE_NOT_PERMIT = 0x0003;
 	public static final int GATT_INVALID_PDU = 0x0004;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_INSUFFICIENT_AUTHENTICATION
 	public static final int GATT_INSUF_AUTHENTICATION = 0x0005;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_REQUEST_NOT_SUPPORTED
 	public static final int GATT_REQ_NOT_SUPPORTED = 0x0006;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_INVALID_OFFSET
 	public static final int GATT_INVALID_OFFSET = 0x0007;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_INSUFFICIENT_AUTHORIZATION
 	public static final int GATT_INSUF_AUTHORIZATION = 0x0008;
 	public static final int GATT_PREPARE_Q_FULL = 0x0009;
 	public static final int GATT_NOT_FOUND = 0x000a;
 	public static final int GATT_NOT_LONG = 0x000b;
 	public static final int GATT_INSUF_KEY_SIZE = 0x000c;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_INVALID_ATTRIBUTE_LENGTH
 	public static final int GATT_INVALID_ATTR_LEN = 0x000d;
 	public static final int GATT_ERR_UNLIKELY = 0x000e;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_INSUFFICIENT_ENCRYPTION
 	public static final int GATT_INSUF_ENCRYPTION = 0x000f;
 	public static final int GATT_UNSUPPORT_GRP_TYPE = 0x0010;
 	public static final int GATT_INSUF_RESOURCE = 0x0011;
@@ -74,6 +82,7 @@ public class GattError {
 	public static final int GATT_SERVICE_STARTED = 0x008c;
 	public static final int GATT_ENCRYPTED_NO_MITM = 0x008d;
 	public static final int GATT_NOT_ENCRYPTED = 0x008e;
+	// https://developer.android.com/reference/android/bluetooth/BluetoothGatt#GATT_CONNECTION_CONGESTED
 	public static final int GATT_CONGESTED = 0x008f;
 	public static final int GATT_CCCD_CFG_ERROR = 0x00FD;
 	public static final int GATT_PROCEDURE_IN_PROGRESS = 0x00FE;
